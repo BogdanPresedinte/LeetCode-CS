59f8dde5b4360fefc3968310237fc2f42ffcf8ac
Bogdan Raducanu <b.c.raducanu@gmail.com>
2024-02-20 11:25:53 +0200

    Code cleanup.

--- LeetCode/0001-0050/001-TwoSum.cs
+++ LeetCode/0001-0050/001-TwoSum.cs
@@ -15,7 +15,8 @@ namespace LeetCode
         /// You may assume that each input would have exactly one solution, and you may not use the same element twice.
         /// You can return the answer in any order.
         /// </summary>
-        public int[] TwoSum(int[] nums, int target) {
+        public int[] TwoSum(int[] nums, int target)
+        {
             Dictionary<int, int> dic = [];
 
             for (int i = 0; i < nums.Length; i++) {
