874e8fc6b0d00bd77f6aad64d9c22617c554590c
Bogdan Raducanu <b.c.raducanu@gmail.com>
2024-02-20 10:06:05 +0200

    Add comment for TwoSum merhod.

--- LeetCode/0001-0050/001-TwoSum.cs
+++ LeetCode/0001-0050/001-TwoSum.cs
@@ -10,6 +10,11 @@ namespace LeetCode
 
     public class _001_TwoSum
     {
+        /// <summary>
+        /// Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+        /// You may assume that each input would have exactly one solution, and you may not use the same element twice.
+        /// You can return the answer in any order.
+        /// </summary>
         public int[] TwoSum(int[] nums, int target) {
             Dictionary<int, int> dic = [];
 
